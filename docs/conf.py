4b5a2f10d83a04e8866682bd9d60a9c4b7d9b759
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2020-10-30 12:23:26 +0100

    Update github workflows

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,4 @@
-import cloud_sptheme as csp
+import sphinx_rtd_theme as theme
 source_suffix = '.rst'
 master_doc = 'index'
 project = u'whelk'
@@ -6,11 +6,11 @@ copyright = u'2010-2015, Dennis Kaarsemaker'
 version = '2.7'
 release = '2.7.1'
 pygments_style = 'sphinx'
-html_theme = 'cloud'
+html_theme = 'sphinx_rtd_theme'
 html_theme_options = {
     'roottarget': 'index',
     'stickysidebar': False,
 }
-html_theme_path = [csp.get_theme_dir()]
+html_theme_path = [theme.get_html_theme_path()]
 html_show_sourcelink = False
 html_show_sphinx = False
